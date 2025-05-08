f51a55993bd5e1fdce7fbbe895abe8aca760013c
LaurenceMizzi-thebest <laurence.mizzi@students.mq.edu.au>
2025-05-08 15:42:08 +1000

    Final Commit for contact

--- Assets/TilemapGame/Tilesheet.tsx
+++ Assets/TilemapGame/Tilesheet.tsx
@@ -1,4 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeon_Tilesheet" tilewidth="16" tileheight="16" tilecount="1824" columns="18">
+<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeon_Tilesheet" tilewidth="16" tileheight="16" tilecount="198" columns="18">
  <image source="kenney_tiny-battle/Tilemap/tilemap_packed.png" width="288" height="176"/>
+ <tile id="154">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="16" height="14">
+    <properties>
+     <property name="unity:IsTrigger" value="true"/>
+     <property name="unity:layer" value="Hazard"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="166">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.144567" y="0.0361419" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
